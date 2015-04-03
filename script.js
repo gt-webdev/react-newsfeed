bbd23325902d0bc0c496bb9c9361db49b4b37b3f
Jennifer Hwang <jmhwang7@gmail.com>
2015-04-03 17:58:44 -0400

    Added React classes for different components.

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
-var ArticleComponent = React.createClass({
+// Allows user to change rss feed that supplies articles that are shown.
+var FeedInput = React.createClass({
 	render: function() {
 		return(
-			<h1>Hello World</h1>
+			<h1>Feed URL input box</h1>
+		);
+	}
+});
+
+var ArticleCard = React.createClass({
+	render: function() {
+		return (
+			<li>
+				<div>Image</div>
+				<h3>React Native</h3>
+				<p>Author</p>
+			</li>
+		);
+	}
+});
+
+// List of unread articles
+var UnreadArticles = React.createClass({
+	render: function() {
+		return(
+			<ul>
+				<ArticleCard />
+			</ul>
+		);
+	}
+});
+
+// List of read articles
+var ReadArticles = React.createClass({
+	render: function() {
+		return(
+			<ul>
+				<ArticleCard />
+			</ul>
+		);
+	}
+});
+
+// Lists of Articles
+var ArticlesLists = React.createClass({
+	render: function() {
+		return(
+			// TODO: Reduce duplication.
+			<div>
+				<UnreadArticles />
+				<ReadArticles />
+			</div>
+		);
+	}
+});
+
+//Main component that contains all the components for the news feed.
+var NewsFeedComponent = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<FeedInput />
+				<ArticlesLists />
+			</div>
 		);
 	}
 });
 React.render(
-	<ArticleComponent />,
+	<NewsFeedComponent />,
 	document.getElementById('content')
 );
